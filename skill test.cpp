43469155c4a6b1a091af7866d78a217d0251f4c8
renzrenzrenzrenz69 <47804372+renzrenzrenzrenz69@users.noreply.github.com>
2019-03-13 13:43:00 +0800

    Update skill test.cpp

--- skill test.cpp
+++ skill test.cpp
@@ -11,11 +11,11 @@ int main ()
 	cin >> P;
 	cout << "Number of hours you work for each week" << endl;
 	cin >> H;
-	cout << "income before tax     "<< P * H << "     income after tax     " << P*H - (P * H *.14) << endl; 
-	cout << "The money you spent on clothes and other accessories     "<< (P*H - (P * H *.14)) *.10 << endl; 
-	cout << "The money you spent on school supplies    "<< (P*H - (P * H *.14)) *.010 << endl; 
-	cout << "The money you spent on buying saving bonds     "<< (P*H - (P * H *.14)) *.25 << endl; 
-	cout << "The money your parents spent on buying  you additional saving bonds     "<< ((P*H - (P * H *.14)) *.25)/2 << endl; 
+	cout << "income before tax     "<< P * H *5 << "     income after tax     " << P*H*5 - ((P * H * 5) *.14) << endl; 
+	cout << "The money you spent on clothes and other accessories     "<< (P*H*5 - ((P * H * 5)*.14)) *.10 << endl; 
+	cout << "The money you spent on school supplies    "<< (P*H*5 - ((P * H * 5)*.14)) *.010 << endl; 
+	cout << "The money you spent on buying saving bonds     "<< (P*H*5- ((P * H * 5).14)) *.25 << endl; 
+	cout << "The money your parents spent on buying  you additional saving bonds     "<< ((P*H*5 - (P * H *5) *.14)) *.25)/2 << endl; 
 	
 	
 	
